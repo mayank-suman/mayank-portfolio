38a192c853cbc8f50469f998374172bb2bf782fd
Mayank Suman <mayank@simpplr.com>
2021-03-30 17:50:12 +0530

    refactor to remove extra fn calls

--- js/main.js
+++ js/main.js
@@ -1,101 +1,97 @@
-function main() {
-  (function () {
-    'use strict';
+'use strict';
+(function () {
+  $('a.page-scroll').click(function () {
+    if (
+      location.pathname.replace(/^\//, '') ==
+        this.pathname.replace(/^\//, '') &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate(
+          {
+            scrollTop: target.offset().top - 40,
+          },
+          900
+        );
+        return false;
+      }
+    }
+  });
 
-    $('a.page-scroll').click(function () {
-      if (
-        location.pathname.replace(/^\//, '') ==
-          this.pathname.replace(/^\//, '') &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html,body').animate(
-            {
-              scrollTop: target.offset().top - 40,
+  // affix the navbar after scroll below header
+  $('#nav').affix({
+    offset: {
+      top: $('header').height(),
+    },
+  });
+
+  // skills chart
+  $(document).ready(function (e) {
+    //var windowBottom = $(window).height();
+    var index = 0;
+    $(document).scroll(function () {
+      var top = $('#skills').height() - $(window).scrollTop();
+      // console.log(top)
+      if (top < -300) {
+        if (index == 0) {
+          $('.chart').easyPieChart({
+            easing: 'easeOutBounce',
+            onStep: function (from, to, percent) {
+              $(this.el).find('.percent').text(Math.round(percent));
             },
-            900
-          );
-          return false;
+          });
         }
+        index++;
       }
     });
+    //console.log(nagativeValue)
+  });
 
-    // affix the navbar after scroll below header
-    $('#nav').affix({
-      offset: {
-        top: $('header').height(),
+  // Portfolio isotope filter
+  $(window).on('load', function () {
+    var $container = $('.portfolio-items');
+    $container.isotope({
+      filter: '*',
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false,
       },
     });
-
-    // skills chart
-    $(document).ready(function (e) {
-      //var windowBottom = $(window).height();
-      var index = 0;
-      $(document).scroll(function () {
-        var top = $('#skills').height() - $(window).scrollTop();
-        // console.log(top)
-        if (top < -300) {
-          if (index == 0) {
-            $('.chart').easyPieChart({
-              easing: 'easeOutBounce',
-              onStep: function (from, to, percent) {
-                $(this.el).find('.percent').text(Math.round(percent));
-              },
-            });
-          }
-          index++;
-        }
-      });
-      //console.log(nagativeValue)
-    });
-
-    // Portfolio isotope filter
-    $(window).on('load', function () {
-      var $container = $('.portfolio-items');
+    $('.cat a').click(function () {
+      $('.cat .active').removeClass('active');
+      $(this).addClass('active');
+      var selector = $(this).attr('data-filter');
       $container.isotope({
-        filter: '*',
+        filter: selector,
         animationOptions: {
           duration: 750,
           easing: 'linear',
           queue: false,
         },
       });
-      $('.cat a').click(function () {
-        $('.cat .active').removeClass('active');
-        $(this).addClass('active');
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-          filter: selector,
-          animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false,
-          },
-        });
-        return false;
+      return false;
+    });
+  });
+
+  // CounterUp
+  $(document).ready(function ($) {
+    if ($('span.count').length > 0) {
+      $('span.count').counterUp({
+        delay: 10, // the delay time in ms
+        time: 1500, // the speed time in ms
       });
-    });
+    }
+  });
 
-    // CounterUp
-    $(document).ready(function ($) {
-      if ($('span.count').length > 0) {
-        $('span.count').counterUp({
-          delay: 10, // the delay time in ms
-          time: 1500, // the speed time in ms
-        });
-      }
-    });
+  // Pretty Photo
+  $("a[rel^='prettyPhoto']").prettyPhoto({
+    social_tools: false,
+  });
 
-    // Pretty Photo
-    $("a[rel^='prettyPhoto']").prettyPhoto({
-      social_tools: false,
-    });
-
-    /* 
+  /* 
   const headerEle = document.querySelector("#header");
   const introEle = document.querySelector(".intro");
 
@@ -106,6 +102,4 @@ function main() {
     introEle.style.backgroundPositionY = -e.offsetY * 0.02 + "px";
   }); 
   */
-  })();
-}
-main();
+})();
