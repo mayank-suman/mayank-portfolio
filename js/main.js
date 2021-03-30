a94a8a7526e37c5ea33e25dbddafffaf6085d73b
Mayank Suman <mayank@simpplr.com>
2021-03-30 17:55:57 +0530

    remove all deprecate warnings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 (function () {
-  $('a.page-scroll').click(function () {
+  $('a.page-scroll').on('click', function () {
     if (
       location.pathname.replace(/^\//, '') ==
         this.pathname.replace(/^\//, '') &&
@@ -28,26 +28,24 @@
   });
 
   // skills chart
-  $(document).ready(function (e) {
-    //var windowBottom = $(window).height();
-    var index = 0;
-    $(document).scroll(function () {
-      var top = $('#skills').height() - $(window).scrollTop();
-      // console.log(top)
-      if (top < -300) {
-        if (index == 0) {
-          $('.chart').easyPieChart({
-            easing: 'easeOutBounce',
-            onStep: function (from, to, percent) {
-              $(this.el).find('.percent').text(Math.round(percent));
-            },
-          });
-        }
-        index++;
+  //var windowBottom = $(window).height();
+  var index = 0;
+  $(document).on('scroll', function () {
+    var top = $('#skills').height() - $(window).scrollTop();
+    // console.log(top)
+    if (top < -300) {
+      if (index == 0) {
+        $('.chart').easyPieChart({
+          easing: 'easeOutBounce',
+          onStep: function (from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+          },
+        });
       }
-    });
-    //console.log(nagativeValue)
+      index++;
+    }
   });
+  //console.log(nagativeValue)
 
   // Portfolio isotope filter
   $(window).on('load', function () {
@@ -77,14 +75,12 @@
   });
 
   // CounterUp
-  $(document).ready(function ($) {
-    if ($('span.count').length > 0) {
-      $('span.count').counterUp({
-        delay: 10, // the delay time in ms
-        time: 1500, // the speed time in ms
-      });
-    }
-  });
+  if ($('span.count').length > 0) {
+    $('span.count').counterUp({
+      delay: 10, // the delay time in ms
+      time: 1500, // the speed time in ms
+    });
+  }
 
   // Pretty Photo
   $("a[rel^='prettyPhoto']").prettyPhoto({
