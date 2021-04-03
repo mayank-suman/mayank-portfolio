e55278a0ebbaf76cab4158d74c23bd06d4ccbed0
Mayank Suman <mayank@simpplr.com>
2021-04-03 14:36:44 +0530

    remove jquery implicit import

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 import '../sass/index.scss';
 
-var jquery = require('jquery');
-window.$ = window.jQuery = jquery;
+// require('jquery-migrate');
+// jQuery.migrateMute = true;
 
-require('jquery-migrate');
-jQuery.migrateMute = true;
-
-import smoothscroll from 'smoothscroll-for-websites';
+// import smoothscroll from 'smoothscroll-for-websites';
 
 import './main';
