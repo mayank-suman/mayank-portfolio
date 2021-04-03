dddc501438b7d06f70578d4d6d6c9d4798ab161c
Mayank Suman <mayank@simpplr.com>
2021-04-03 17:54:07 +0530

    change dist path

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/js/app.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, '../dist'),
   },
   module: {
     rules: [
