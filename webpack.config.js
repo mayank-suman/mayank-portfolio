eba45e72bae56ab8b4a1a89ec136dcf398d570a2
Mayank Suman <mayank@simpplr.com>
2021-04-03 16:17:12 +0530

    change index.html path also

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: 'src/index.html',
     }),
     new webpack.ProvidePlugin({
       $: 'jquery',
